512b14cf80ed04bab9dc043ab601d67b040c6394
Everett Kropf <ehkropf@gmail.com>
2022-12-30 13:21:45 +0000

    Moar file cleanup

--- keyboards/gmmk/pro/rev1/iso/keymaps/ekropf/keymap.c
+++ keyboards/gmmk/pro/rev1/iso/keymaps/ekropf/keymap.c
@@ -26,12 +26,12 @@ enum local_keycodes
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
-//      `        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
-//      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]                          PgUp
-//      Caps     A        S        D        F        G        H        J        K        L        ;        '        #        Enter             PgDn
-//      Sh_L     \        Z        X        C        V        B        N        M        ,        .        /                 Sh_R     Up       End
-//      Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
+//      ESC        F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12      Prt           Rotary(Mute)
+//      `          1        2        3        4        5        6        7        8        9        0         -       (=)      BackSpc           Del
+//      Tab        Q        W        E        R        T        Y        U        I        O        P        [        ]                          PgUp
+//      Ct_l/ESC   A        S        D        F        G        H        J        K        L        ;        '        #        Enter             PgDn
+//      Sh_L       \        Z        X        C        V        B        N        M        ,        .        /                 Sh_R     Up       End
+//      FN         Alt_L    GUI_L                      SPACE                               GUI_R    Alt_R    Ct_R     Left     Down     Right
 
     // The FN key by default maps to a momentary toggle to layer 1 to provide access to the RESET key (to put the board into bootloader mode). Without
     // this mapping, you have to open the case to hit the button on the bottom of the PCB (near the USB cable attachment) while plugging in the USB
@@ -41,13 +41,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // To put the keyboard in bootloader mode, use FN+#. If you accidentally put it into bootloader, you can just unplug the USB cable and
     // it'll be back to normal when you plug it back in.
     [0] = LAYOUT(
-        KC_ESC,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   RGB_TOG, KC_MUTE,
-        KC_NUBS,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_HOME,
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,           KC_PGUP,
+        KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   RGB_TOG, KC_MUTE,
+        KC_NUBS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_HOME,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,           KC_PGUP,
         MT(MOD_LCTL, KC_ESC),
-                        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,  KC_PGDN,
-        KC_LSFT,        KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,   KC_END,
-        MO(1),          KC_LALT, KC_LGUI,                            KC_SPC,                    KC_RGUI, KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RGHT
+                   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,  KC_PGDN,
+        KC_LSFT,   KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,   KC_END,
+        MO(1),     KC_LALT, KC_LGUI,                            KC_SPC,                    KC_RGUI, KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT(
